2fc82433d83e704e746925df2e737ea91cb8c15f
Lorena <lorena.g.saar@gmail.com>
2020-08-30 14:30:20 +0200

    refactoring

--- APIPython.py
+++ APIPython.py
@@ -21,76 +21,85 @@ def get_pokemons(url='http://pokeapi.co/api/v2/pokemon/', offset=0):
         if results:
             for pokemon in results:
                 name = pokemon['name']
-                PokemonUrl = pokemon['url']
+                pokemon_url = pokemon['url']
                 
-                PokeResponse = requests.get(PokemonUrl, params=args)
-               
-                ReturnNode = PokeResponse.json()
-               
-                
-               
-               #Pokemon Types
-                typelist = [] 
-                for PokemonType in ReturnNode['types']:
-                    typelist.append(PokemonType['type']['name'])
-                    
-                PokemonTypeText = ' and '.join(typelist)
-                
-                #Pokemon Eggs
-                PokeSpeciesUrl = ReturnNode['species']['url']
-                PokeSpecies = requests.get(PokeSpeciesUrl, params=args)
-                ReturnPokeSpecies = PokeSpecies.json()
-                
-                Arr_eggGroups=[]
-                for Egggroup in ReturnPokeSpecies['egg_groups']:
-                    Arr_eggGroups.append(Egggroup['name'])
-                
-                PokemonEgg = ' and '.join(Arr_eggGroups)    
-                
-                
-                
-                #PokemonGeneration
-                
-                PokeGen = ReturnPokeSpecies['generation']['name']
-                            
-                
-                
-                #Pokemon Stats
-                attack=0
-                defense=0
-                speed=0
-                for node in ReturnNode['stats']:
-                    
-                    if node['stat']['name'] == 'attack':
-                        attack = node['base_stat']
-                    if node['stat']['name'] == 'defense':
-                        defense = node['base_stat']
-                    if node['stat']['name'] == 'speed':
-                        speed = node['base_stat']
-                    
-                #Pokemon Location    
-                LocationUrl = ReturnNode['location_area_encounters']
-                LocationResponse = requests.get(LocationUrl, params = args)
-                
-                LocationNode = LocationResponse.json()
-                
-                LocalList=[]
-                for PokeLocation in LocationNode:
-                    LocalList.append(PokeLocation['location_area']['name'])
-                
-                PokemonLocation = ' and '.join(LocalList)    
-                
-                
-                
+                pokemon_info = get_pokemon_info(pokemon_url)    
+                pokemon_type_text = get_pokemon_type(pokemon_info)
+                pokemon_species = get_pokemon_species(pokemon_info)
+                pokemon_egg = get_pokemon_egg(pokemon_species)
+                pokemon_gen = get_pokemon_gen(pokemon_species)
+                pokemon_location = get_pokemon_location(pokemon_info)
+                pokemon_stats = get_pokemon_stats(pokemon_info)
+                     
                 #Writing my data on csv    
                 file = open('pokelist.csv','a')
-                file.write(name + ',' + PokemonTypeText + ',' + str(attack) + ',' + str(defense) + ',' + str(speed) +  ',' + str(PokemonLocation) +  ',' +  PokemonEgg +   ',' +  PokeGen + '\n')
+                file.write(name + ',' + pokemon_type_text + ',' + str(pokemon_stats['attack']) + ',' + str(pokemon_stats['defense']) + ',' + str(pokemon_stats['speed']) +  ',' + pokemon_location +  ',' +  pokemon_egg +   ',' +  pokemon_gen + '\n')
                 file.close()
                 
     next = input("Keep loading? [Y/N]").lower()
     if next == 'y':
         get_pokemons(offset=offset+20)
+
+def get_pokemon_info(url):
+    response = requests.get(url)          
+    return response.json()   
+
+def get_pokemon_type(pokemon_info):   
+    typelist = [] 
+    for type_node in pokemon_info['types']:
+        typelist.append(type_node['type']['name'])
+        
+    result = ' and '.join(typelist)
+    return result
+
+def get_pokemon_species(pokemon_info):
+    poke_species_url = pokemon_info['species']['url']
+    response = requests.get(poke_species_url)
+    species_info = response.json()
+    return species_info
+
+def get_pokemon_egg(species_info):
+    egg_groups=[]
+    for egg in species_info['egg_groups']:
+        egg_groups.append(egg['name'])
+    
+    result = ' and '.join(egg_groups)    
+    return result
+
+def get_pokemon_gen(species_info):
+    result = species_info['generation']['name']
+    return result
+
+def get_pokemon_location(pokemon_info):
+    location_url = pokemon_info['location_area_encounters']
+    response = requests.get(location_url)
+    
+    location_info = response.json()
+    
+    local_list=[]
+    for PokeLocation in location_info:
+        local_list.append(PokeLocation['location_area']['name'])
+    
+    result = ' and '.join(local_list)    
+    return result
+
+def get_pokemon_stats(pokemon_info):    
+    result = {
+        "attack": 0,    
+        "defense": 0,
+        "speed": 0
+    }
+    
+    for node in pokemon_info['stats']:
        
+        if node['stat']['name'] == 'attack':
+            result['attack'] = node['base_stat']
+        if node['stat']['name'] == 'defense':
+            result['defense'] = node['base_stat']
+        if node['stat']['name'] == 'speed':
+            result['speed'] = node['base_stat']
+                    
+    return result                
 
 if __name__=='__main__':
     #url='http://pokeapi.co/api/v2/pokemon-form/'
